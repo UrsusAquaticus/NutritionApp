3cf2034af7e0a46709b986a37597d76f506e8b94
Zach <zach.hucker@gmail.com>
2022-05-16 18:24:42 +1200

    Added profile page connection

---
+++ NutritionApp/NutritionApp/ViewModels/Pages/ProfilesPageViewModel.cs
@@ -0,0 +1,89 @@
+﻿using NutritionApp.Models;
+using NutritionApp.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace NutritionApp.ViewModels
+{
+    public class ProfilesPageViewModel : BaseViewModel
+    {
+        //https://medium.com/swlh/xamarin-forms-mvvm-how-to-work-with-sqlite-db-c-xaml-26fcae303edd
+        //Look here for how to implement the custom interface 'IPageService'
+        private ProfileViewModel _selectedProfile;
+        private IDataStore<Profile> _profileStore;
+        //private IPageService _pageService;
+
+        private bool _isDataLoaded;
+
+        public ObservableCollection<ProfileViewModel> Profiles { get; private set; }
+            = new ObservableCollection<ProfileViewModel>();
+
+        public ProfileViewModel SelectedProfile
+        {
+            get { return _selectedProfile; }
+            set { SetValue(ref _selectedProfile, value); }
+        }
+
+        public ICommand LoadDataCommand { get; private set; }
+        public ICommand AddProfileCommand { get; private set; }
+        public ICommand SelectProfileCommand { get; private set; }
+        public ICommand DeleteProfileCommand { get; private set; }
+        public ICommand CallProfileCommand { get; private set; }
+
+        //public ProfilesPageViewModel(IDataStore<Profile> profileStore, IPageService pageService)
+        public ProfilesPageViewModel(IDataStore<Profile> profileStore)
+        {
+            _profileStore = profileStore;
+            //_pageService = pageService;
+
+            LoadDataCommand = new Command(async () => await LoadData());
+            AddProfileCommand = new Command(async () => await AddProfile());
+            SelectProfileCommand = new Command<ProfileViewModel>(async c => await SelectProfile(c));
+            DeleteProfileCommand = new Command<ProfileViewModel>(async c => await DeleteProfile(c));
+        }
+
+        private async Task LoadData()
+        {
+            if (_isDataLoaded)
+                return;
+            await _profileStore.AddAsync(new Profile {Id = 1, Name="Zach", Pregnant=true });
+            _isDataLoaded = true;
+            var profiles = await _profileStore.GetAsync();
+            foreach (var profile in profiles)
+                Profiles.Add(new ProfileViewModel(profile));
+        }
+
+        private async Task AddProfile()
+        {
+            //await _pageService.PushAsync(new ProfilesDetailPage(new ProfileViewModel()));
+            await new Task(() => { Console.WriteLine("Hasn't been implemented yet lol"); });
+        }
+
+        private async Task SelectProfile(ProfileViewModel profile)
+        {
+            if (profile == null)
+                return;
+
+            SelectedProfile = null;
+            //await _pageService.PushAsync(new ProfilesDetailPage(profile));
+            await new Task(() => { Console.WriteLine("Hasn't been implemented yet lol"); });
+        }
+
+        private async Task DeleteProfile(ProfileViewModel profileViewModel)
+        {
+            //if (await _pageService.DisplayAlert("Warning", $"Are you sure you want to delete {profileViewModel.FullName}?", "Yes", "No"))
+            //{
+            //    Profiles.Remove(profileViewModel);
+
+            //    var profile = await _profileStore.GetProfile(profileViewModel.Id);
+            //    await _profileStore.DeleteProfile(profile);
+            //}
+            await new Task(() => { Console.WriteLine("Hasn't been implemented yet lol"); });
+        }
+    }
+}
--- NutritionApp/NutritionApp/Views/ProfilePage.xaml.cs
+++ NutritionApp/NutritionApp/Views/ProfilePage.xaml.cs
@@ -10,46 +10,70 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using System.Collections.ObjectModel;
+using NutritionApp.Persistence;
 
 namespace NutritionApp.Views
 {
     public partial class ProfilePage : ContentPage
     {
-        ProfileViewModel _viewModel;
 
         public ProfilePage()
         {
+            var profileStore = new SQLiteProfileStore(DependencyService.Get<ISQLiteDb>());
+            //var pageService = new PageService();
+            ViewModel = new ProfilesPageViewModel(profileStore);
             InitializeComponent();
-
-            BindingContext = _viewModel = new ProfileViewModel();
-            CreateProfileList();
         }
-
-        ObservableCollection<Profile> profiles = new ObservableCollection<Profile>();
-        public ObservableCollection<Profile> Profiles { get { return profiles; } }
-
-        private void CreateProfileList()
-        {
-            profileList.ItemsSource = profiles;
-            profiles.Add(new Profile { Name = "Dani JS" });
-            profiles.Add(new Profile { Name = "Zach" });
-            profiles.Add(new Profile { Name = "Sam" });
-            profiles.Add(new Profile { Name = "Jethro" });
-        }
-
         protected override void OnAppearing()
         {
+            ViewModel.LoadDataCommand.Execute(null);
             base.OnAppearing();
-            //_viewModel.OnAppearing();
         }
-
-        private async void New_Profile_Clicked(object sender, EventArgs e)
+        void OnProfileSelected(object sender, SelectedItemChangedEventArgs e)
         {
-            // register route
-            Routing.RegisterRoute("NewProfilePage", typeof(NewProfilePage));
-
-            await Shell.Current.GoToAsync("NewProfilePage");
-
+            ViewModel.SelectProfileCommand.Execute(e.SelectedItem);
         }
+        public ProfilesPageViewModel ViewModel
+        {
+            get { return BindingContext as ProfilesPageViewModel; }
+            set { BindingContext = value; }
+        }
+
+
+        //ProfilesPageViewModel _viewModel;
+        //public ProfilePage()
+        //{
+        //    InitializeComponent();
+
+        //    //BindingContext = _viewModel = new ProfilePageViewModel();
+        //    CreateProfileList();
+        //}
+
+        //ObservableCollection<Profile> profiles = new ObservableCollection<Profile>();
+        //public ObservableCollection<Profile> Profiles { get { return profiles; } }
+
+        //private void CreateProfileList()
+        //{
+        //    profileList.ItemsSource = profiles;
+        //    profiles.Add(new Profile { Name = "Dani JS" });
+        //    profiles.Add(new Profile { Name = "Zach" });
+        //    profiles.Add(new Profile { Name = "Sam" });
+        //    profiles.Add(new Profile { Name = "Jethro" });
+        //}
+
+        //protected override void OnAppearing()
+        //{
+        //    base.OnAppearing();
+        //    //_viewModel.OnAppearing();
+        //}
+
+        //private async void New_Profile_Clicked(object sender, EventArgs e)
+        //{
+        //    // register route
+        //    Routing.RegisterRoute("NewProfilePage", typeof(NewProfilePage));
+
+        //    await Shell.Current.GoToAsync("NewProfilePage");
+
+        //}
     }
 }
