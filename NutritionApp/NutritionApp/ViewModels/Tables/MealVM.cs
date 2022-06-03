446e3dcdcd5f8d4136b579adb7aa9abc7691920b
Zach <zach.hucker@gmail.com>
2022-06-03 16:12:02 +1200

    Updated view models so hopefully useable

--- NutritionApp/NutritionApp/ViewModels/Tables/MealVM.cs
+++ NutritionApp/NutritionApp/ViewModels/Tables/MealVM.cs
@@ -8,10 +8,25 @@ namespace NutritionApp.ViewModels.Tables
     class MealVM : BaseViewModel
     {
         private int id;
-        private ObservableCollection<ProfileViewModel> mealIngredient;
-
-        //
         private string name;
         private float servingSizeGrams;
+        private ObservableCollection<MealIngredientVM> mealIngredients;
+
+        public MealVM()
+        {
+        }
+
+        public MealVM(int id, string name, float servingSizeGrams, ObservableCollection<MealIngredientVM> mealIngredients)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.ServingSizeGrams = servingSizeGrams;
+            this.MealIngredients = mealIngredients;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => SetValue(ref name, value); }
+        public float ServingSizeGrams { get => servingSizeGrams; set => SetValue(ref servingSizeGrams, value); }
+        public ObservableCollection<MealIngredientVM> MealIngredients { get => mealIngredients; set => SetValue(ref mealIngredients, value); }
     }
 }
