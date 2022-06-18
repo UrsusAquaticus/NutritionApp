efa7dacb601fd564e9e0dc2569e5ca5e0113f6db
Danielle Smith <1827190@studentmail.ucol.ac.nz>
2022-06-18 21:18:58 +1200

    removed FFImageLoader, fixed icons

--- NutritionApp/NutritionApp.Android/MainActivity.cs
+++ NutritionApp/NutritionApp.Android/MainActivity.cs
@@ -4,7 +4,6 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
-using FFImageLoading.Forms.Platform;
 
 namespace NutritionApp.Droid
 {
@@ -19,8 +18,6 @@ namespace NutritionApp.Droid
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            FFImageLoading.Forms.Platform.CachedImageRenderer.Init(enableFastRenderer: true);
-            CachedImageRenderer.InitImageViewHandler();
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
--- NutritionApp/NutritionApp.iOS/AppDelegate.cs
+++ NutritionApp/NutritionApp.iOS/AppDelegate.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using FFImageLoading.Forms.Platform;
 using Foundation;
 using UIKit;
 
@@ -23,8 +22,7 @@ namespace NutritionApp.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
-            FFImageLoading.Forms.Platform.CachedImageRenderer.Init();
-            CachedImageRenderer.InitImageSourceHandler();
+
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
